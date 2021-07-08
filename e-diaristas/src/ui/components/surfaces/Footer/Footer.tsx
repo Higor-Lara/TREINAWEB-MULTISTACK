1c38c67498e5d3f5acb2a4765714ca32074f059a
Higor Lara <higorv.lara@gmail.com>
2021-07-07 23:41:01 -0300

    a

---
+++ e-diaristas/src/ui/components/surfaces/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  FooterStyled,
+  FooterContainer,
+  FooterTitle,
+  AppList,
+} from "./Footer.style";
+import { Typography, Box } from "@material-ui/core";
+
+const Footer = () => {
+  return (
+    <FooterStyled>
+      <FooterContainer>
+        <Box sx={{ maxWidth: "400px" }}>
+          <FooterTitle>Meu titulo</FooterTitle>
+          <Typography>
+            E-Diaristas te ajuda a encontrar um profissional perfeito para
+            realizar a limpeza da sua casa. Garantimos a melhor profissional com
+            total segurança e praticidade! São milhares de clientes satisfeitos
+            por todo o país.
+          </Typography>
+        </Box>
+        <Box>
+          <FooterTitle>Baixe nossos aplicativos</FooterTitle>
+          <AppList>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/app-store.png"} alt={"App Store"} />
+              </a>
+            </li>
+            <li>
+              <a href={"/"} target={"_blank"} rel={"noopener noreferrer"}>
+                <img src={"/img/logos/google-play.png"} alt={"Google PLay"} />
+              </a>
+            </li>
+          </AppList>
+        </Box>
+      </FooterContainer>
+    </FooterStyled>
+  );
+};
+
+export default Footer;
